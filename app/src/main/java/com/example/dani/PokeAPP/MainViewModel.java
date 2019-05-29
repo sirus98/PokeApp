77a01a0192ed4ef58718d1119b0c8c88aa6f1248
sirus98 <clopez@elpuig.xeill.net>
2019-05-30 01:59:54 +0200

    Proyecto limpio

--- app/src/main/java/com/example/dani/PokeAPP/MainViewModel.java
+++ app/src/main/java/com/example/dani/PokeAPP/MainViewModel.java
@@ -5,20 +5,16 @@ import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
 import android.support.annotation.NonNull;
 
-import com.example.dani.PokeAPP.db.Pokedao;
-import com.example.dani.PokeAPP.db.Pokedb;
 import com.example.dani.PokeAPP.model.Poke;
 
 import java.util.List;
 
 public class MainViewModel extends AndroidViewModel {
     private PokedbRepository pokedbRepository;
-    private Pokedao pokedao;
 
     public MainViewModel(@NonNull Application application) {
         super(application);
         pokedbRepository = new PokedbRepository();
-        pokedao = Pokedb.getDatabase(application).pokedao();
     }
 
     public LiveData<List<Poke>> getPokemons() {
--- app/src/main/java/com/example/dani/PokeAPP/model/Species.java
+++
@@ -1,6 +0,0 @@
-package com.example.dani.PokeAPP.model;
-
-public class Species {
-    public String name;
-    public String url;
-}
