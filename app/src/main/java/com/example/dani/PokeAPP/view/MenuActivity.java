96723f59013167cba58e3fa36988814423763c85
dam2a <eloygomez@elpuig.xeill.net>
2019-05-30 20:56:14 +0200

    añadido shake

--- app/src/main/java/com/example/dani/PokeAPP/view/MenuActivity.java
+++ app/src/main/java/com/example/dani/PokeAPP/view/MenuActivity.java
@@ -1,10 +1,18 @@
 package com.example.dani.PokeAPP.view;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.dani.PokeAPP.R;
@@ -17,10 +25,23 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private SensorManager sensorManager;
+    private float acelVal;  //current acceleration and gravity
+    private float acelLast; //last acceleration and gravity
+    private float shake;    //acceleration value differ from gravity
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensorManager.registerListener(sensorListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+
+        acelVal = sensorManager.GRAVITY_EARTH;
+        acelLast = SensorManager.GRAVITY_EARTH;
+        shake = 0.00f;
 
         findViewById(R.id.ListaPokemon).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,4 +93,29 @@ public class MenuActivity extends AppCompatActivity {
         }
 
     }
+    private final SensorEventListener sensorListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            acelLast = acelVal;
+            acelVal = (float) Math.sqrt((double) (x*x + y*y + z*z));
+            float delta = acelVal - acelLast;
+            shake = shake * 0.9f + delta;
+
+            if (shake > 12 ){
+                Toast toast = Toast.makeText(getApplicationContext(),"No me la menees", Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
